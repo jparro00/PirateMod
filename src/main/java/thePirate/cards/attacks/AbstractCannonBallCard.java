bdcfcb563d6508b6ebc8a8576c048ccadba8d56c
jparro00 <jparro00@gmail.com>
2023-01-12 05:06:29 -0500

    - fixed FreeCannonBallPower not working (not sure how this stopped working)

--- src/main/java/thePirate/cards/attacks/AbstractCannonBallCard.java
+++ src/main/java/thePirate/cards/attacks/AbstractCannonBallCard.java
@@ -1,8 +1,12 @@
 package thePirate.cards.attacks;
 
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import thePirate.cards.AbstractDynamicCard;
+import thePirate.powers.FreeCannonballPower;
 
 public abstract class AbstractCannonBallCard extends AbstractDynamicCard {
 
@@ -10,4 +14,13 @@ public abstract class AbstractCannonBallCard extends AbstractDynamicCard {
         super(id, img, cost, type, color, rarity, target);
     }
 
+    @Override
+    public boolean freeToPlay() {
+
+        if (this.freeToPlayOnce) {
+            return true;
+        } else {
+            return AbstractDungeon.player != null && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT && AbstractDungeon.player.hasPower(FreeCannonballPower.POWER_ID);
+        }
+    }
 }
