016fc6ac0a5d96c7b68e95ee461b81272f05f89a
jparro00 <jparro00@gmail.com>
2023-01-12 06:05:35 -0500

    - Card art  - headbutt color

--- src/main/java/thePirate/cards/attacks/Headbutt.java
+++ src/main/java/thePirate/cards/attacks/Headbutt.java
@@ -1,38 +1,52 @@
 package thePirate.cards.attacks;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.unique.DiscardPileToTopOfDeckAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thePirate.PirateMod;
+import thePirate.cards.AbstractDynamicCard;
 import thePirate.characters.ThePirate;
 
 import static thePirate.PirateMod.makeCardPath;
 
-public class Headbutt extends com.megacrit.cardcrawl.cards.red.Headbutt {
 
-    // STAT DECLARATION
-
+public class Headbutt extends AbstractDynamicCard {
+    private static final CardRarity RARITY = CardRarity.UNCOMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ALL_ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;       //
     public static final CardColor COLOR = ThePirate.Enums.COLOR_GRAY;
+    private static final int COST = 2;
 
-    // /STAT DECLARATION/
-
-    // TEXT DECLARATION
     public static final String ID = PirateMod.makeID(Headbutt.class.getSimpleName());
-    // /TEXT DECLARATION/
+    public static final String IMG = makeCardPath(Headbutt.class.getSimpleName()+".png", TYPE);
 
     public Headbutt() {
-        super();
-        this.color = COLOR;
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseDamage = 9;
     }
 
-/*
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, this.damage, this.damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        addToBot(new DiscardPileToTopOfDeckAction(p));
+    }
+
     public void upgrade() {
         if (!this.upgraded) {
             this.upgradeName();
             this.upgradeDamage(3);
         }
-
     }
-*/
+
+
     public AbstractCard makeCopy() {
         return new Headbutt();
     }
+
 }
