2be60ba74efe1d1dabe413f098d7d88eaef7c624
jparro00 <jparro00@gmail.com>
2023-01-14 15:59:10 -0500

    - added card descriptors to cannon and cannonball cards

--- src/main/java/thePirate/cards/AbstractCannonCard.java
+++ src/main/java/thePirate/cards/AbstractCannonCard.java
@@ -1,8 +1,16 @@
 package thePirate.cards;
 
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import thePirate.PirateMod;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractCannonCard extends AbstractDynamicCard{
 
     private static final CardRarity RARITY = CardRarity.SPECIAL;
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(PirateMod.makeID("CardDescriptors"));
 
     @Override
     public boolean canUpgrade() {
@@ -11,6 +19,13 @@ public abstract class AbstractCannonCard extends AbstractDynamicCard{
 
     public AbstractCannonCard(String id, String img, int cost, CardType type, CardColor color, CardTarget target) {
         super(id, img, cost, type, color, RARITY, target);
+    }
+
+    @Override
+    public List<String> getCardDescriptors() {
+        ArrayList<String> retVal = new ArrayList<>();
+        retVal.add(uiStrings.TEXT[0]);
+        return retVal;
     }
 
 }
--- src/main/java/thePirate/cards/attacks/AbstractCannonBallCard.java
+++ src/main/java/thePirate/cards/attacks/AbstractCannonBallCard.java
@@ -1,14 +1,28 @@
 package thePirate.cards.attacks;
 
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import thePirate.PirateMod;
 import thePirate.cards.AbstractDynamicCard;
 import thePirate.powers.FreeCannonballPower;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class AbstractCannonBallCard extends AbstractDynamicCard {
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(PirateMod.makeID("CardDescriptors"));
 
     public AbstractCannonBallCard(String id, String img, int cost, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
         super(id, img, cost, type, color, rarity, target);
+    }
+
+    @Override
+    public List<String> getCardDescriptors() {
+        ArrayList<String> retVal = new ArrayList<>();
+        retVal.add(uiStrings.TEXT[1]);
+        return retVal;
     }
 
     @Override
