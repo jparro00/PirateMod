9fcf01a35f4e4a50e6061655b86e6d9442d4d46f
jparro00 <jparro00@gmail.com>
2023-01-27 11:49:17 -0500

    Bug: fixed Lures so they don't spawn in non-Pirate runs Util: added method for logging card names

--- src/main/java/thePirate/util/CardUtil.java
+++ src/main/java/thePirate/util/CardUtil.java
@@ -1,8 +1,11 @@
 package thePirate.util;
 
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import org.apache.commons.lang3.StringUtils;
+import thePirate.PirateMod;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,4 +27,28 @@ public class CardUtil {
         }
         return pool;
     }
+
+    public static void printCards(){
+
+        CardGroup pool = new CardGroup(CardGroup.CardGroupType.CARD_POOL);
+        pool.group.addAll(AbstractDungeon.commonCardPool.group);
+        pool.group.addAll(AbstractDungeon.uncommonCardPool.group);
+        pool.group.addAll(AbstractDungeon.rareCardPool.group);
+
+        for (AbstractCard card : pool.group){
+            List output = new ArrayList<>();
+            output.add(card.name);
+            output.add(toProperCase(card.type.name()));
+            output.add(card.rawDescription);
+            output.add(card.cost);
+            output.add(toProperCase(card.rarity.name()));
+            PirateMod.logger.info(StringUtils.join(output,'~'));
+        }
+    }
+
+    static String toProperCase(String s) {
+        return s.substring(0, 1).toUpperCase() +
+                s.substring(1).toLowerCase();
+    }
+
 }
