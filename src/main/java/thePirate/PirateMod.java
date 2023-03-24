dbb5d86513e6ba0fa44c7f9698eb6e25ce586114
jparro00 <jparro00@gmail.com>
2023-03-24 09:21:10 -0400

    Code: updated method to check if user is in combat

--- src/main/java/thePirate/PirateMod.java
+++ src/main/java/thePirate/PirateMod.java
@@ -768,7 +768,7 @@ public class PirateMod implements
 
     public static boolean isInCombat(){
 
-        return CardCrawlGame.isInARun() && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
+        return AbstractDungeon.player != null && CardCrawlGame.isInARun() && AbstractDungeon.currMapNode != null && AbstractDungeon.getCurrRoom() != null && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT;
 
     }
     @Override
