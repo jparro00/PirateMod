d3174134757cab6dc3e6b374dc6164a9a57d0f60
jparro00 <jparro00@gmail.com>
2024-06-19 12:53:11 -0400

    Bug: fixed Greedy Chest to give 1 strength when player has 100 gold UI: added counter to Greedy Chest Change: updated hardcore mode to limit Greedy Chest to a maximum of 3 strength

--- src/main/java/thePirate/relics/GreedyChest.java
+++ src/main/java/thePirate/relics/GreedyChest.java
@@ -9,8 +9,7 @@ import com.megacrit.cardcrawl.relics.AbstractRelic;
 import thePirate.PirateMod;
 import thePirate.util.TextureLoader;
 
-import static thePirate.PirateMod.makeRelicOutlinePath;
-import static thePirate.PirateMod.makeRelicPath;
+import static thePirate.PirateMod.*;
 
 public class GreedyChest extends AbstractPirateRelic {
 
@@ -26,21 +25,27 @@ public class GreedyChest extends AbstractPirateRelic {
     private static final Texture IMG = TextureLoader.getTexture(makeRelicPath(GreedyChest.class.getSimpleName() + ".png"));
     private static final Texture OUTLINE = TextureLoader.getTexture(makeRelicOutlinePath(GreedyChest.class.getSimpleName() + ".png"));
 
+    public static final int HC_MAX_STRENGTH = 3;
     public boolean isActive;
 
     public GreedyChest(){
-        super(ID, IMG,OUTLINE,RelicTier.RARE, LandingSound.CLINK);
+        super(ID, IMG,OUTLINE,RelicTier.RARE, LandingSound.CLINK, isHardcore());
+        calculateCounter();
     }
 
 
     public void atBattleStart() {
+        calculateCounter();
         this.isActive = false;
         int gold = AbstractDungeon.player.gold;
         this.addToBot(new AbstractGameAction() {
             public void update() {
-                if (!isActive && gold > 100) {
+                if (!isActive && gold >= 100) {
+                    int amount = gold/100;
+                    //limit to max strength if hardcore mode enabled
+                    amount = hardcore && amount > HC_MAX_STRENGTH ? HC_MAX_STRENGTH : amount;
                     flash();
-                    AbstractDungeon.player.addPower(new StrengthPower(AbstractDungeon.player, gold/100));
+                    AbstractDungeon.player.addPower(new StrengthPower(AbstractDungeon.player, amount));
                     this.addToTop(new RelicAboveCreatureAction(AbstractDungeon.player, GreedyChest.this));
                     isActive = true;
                     AbstractDungeon.onModifyPower();
@@ -66,4 +71,27 @@ public class GreedyChest extends AbstractPirateRelic {
         return new GreedyChest();
     }
 
+    public void calculateCounter(){
+        if (AbstractDungeon.player != null){
+            int gold = AbstractDungeon.player.gold;
+            int amount = gold >= 100 ? gold/100 : -1;
+            //limit to max strength if hardcore mode enabled
+            amount = hardcore && amount > HC_MAX_STRENGTH ? HC_MAX_STRENGTH : amount;
+            setCounter(amount);
+        }
+    }
+    @Override
+    public void onGainGold() {
+        calculateCounter();
+    }
+
+    @Override
+    public void onLoseGold() {
+        calculateCounter();
+    }
+
+    @Override
+    public void onSpendGold() {
+        calculateCounter();
+    }
 }
